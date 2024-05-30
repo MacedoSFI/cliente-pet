83678fede73cccc8ceb08ede620055ee6eca854a
felipe.nomade@yahoo.com <felipe.nomade@yahoo.com>
2024-05-30 17:29:06 -0300

    fim crud

---
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetAlteracaoRequest.java
@@ -0,0 +1,43 @@
+package br.com.petz.clientepet.pet.application.api;
+
+import java.time.LocalDate;
+
+import br.com.petz.clientepet.pet.domain.Porte;
+import br.com.petz.clientepet.pet.domain.SexoPet;
+import br.com.petz.clientepet.pet.domain.TipoPet;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class PetAlteracaoRequest {
+	
+	@NotBlank
+	private String nomePet;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private Porte porte;
+	
+	@NotNull
+	private TipoPet tipo;
+	
+	private String microchip;
+	
+	@NotBlank
+	private String raca;
+	
+	@NotNull	
+	@Enumerated(EnumType.STRING)
+	private SexoPet sexo;
+	
+	private String pelagemCor;
+	@NotNull
+	private LocalDate dataNascimento;
+	
+	private String rga;
+	
+	private Integer peso;
+}
